722a493ad1a95577c47b30900fac5c8ed4678697
Daniil Popov <popov@example.com>
2023-11-08 18:22:20 +0300

    Интефрейс карточки товара

--- app/src/main/java/com/example/shoppinglist/add_item_screen/UiAddItem.kt
+++ app/src/main/java/com/example/shoppinglist/add_item_screen/UiAddItem.kt
@@ -1,8 +1,11 @@
 package com.example.shoppinglist.add_item_screen
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,11 +24,13 @@ import androidx.compose.ui.geometry.Size
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.AddItem
+import com.example.shoppinglist.ui.theme.Yellow
 
 @Composable
 fun UiAddItem(
@@ -42,7 +47,7 @@ fun UiAddItem(
             .drawWithContent {
                 drawContent()
                 drawRect(
-                    color = if (item.priority) Color.Red else Color.Gray,
+                    color = if (item.priority) Yellow else Color.Gray,
                     topLeft = Offset(0f, size.height - 4.dp.toPx()),
                     size = Size(size.width, 4.dp.toPx())
                 )
@@ -58,38 +63,48 @@ fun UiAddItem(
                     .padding(start = 10.dp),
                 text = item.name,
                 fontSize = 12.sp,
+                textAlign = TextAlign.Left
             )
-            if (item.sum > 0){
-                Text(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(start = 10.dp),
-                    text = item.count.toString(),
-                    fontSize = 12.sp,
-                )
-                Text(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(start = 10.dp),
-                    text = item.price.toString(),
-                    fontSize = 12.sp,
-                )
-            }
-            Checkbox(
-                checked = item.isCheck,
-                onCheckedChange = { isChecked ->
-                    onEvent(AddItemEvent.OnCheckedChange(item.copy(isCheck = isChecked)))
-                }
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp),
+                text = if (item.sum>0) item.count.toString() else "",
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center
             )
-            IconButton(
-                onClick = {
-                    onEvent(AddItemEvent.OnDelete(item))
-                }
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp),
+                text = if (item.sum>0) item.price.toString() else "",
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center
+            )
+            Box(
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .weight(1f)
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.delete_icon),
-                    contentDescription = "Delete"
-                )
+                Row(horizontalArrangement = Arrangement.SpaceAround) {
+                    Checkbox(
+                        checked = item.isCheck,
+                        onCheckedChange = { isChecked ->
+                            onEvent(AddItemEvent.OnCheckedChange(item.copy(isCheck = isChecked)))
+                        }
+                    )
+                    IconButton(
+                        onClick = {
+                            onEvent(AddItemEvent.OnDelete(item))
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.delete_icon),
+                            contentDescription = "Delete"
+                        )
+                    }
+                }
+
             }
         }
     }
