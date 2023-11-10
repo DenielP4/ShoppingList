1463d0c1cfa98f7cd406af5b591785629033d883
Daniil Popov <popov@example.com>
2023-11-10 19:47:23 +0300

    EN -> Ru

--- app/src/main/java/com/example/shoppinglist/main_screen/BottomNavItem.kt
+++ app/src/main/java/com/example/shoppinglist/main_screen/BottomNavItem.kt
@@ -4,9 +4,9 @@ import com.example.shoppinglist.R
 import com.example.shoppinglist.utils.Routes
 
 sealed class BottomNavItem(val title: String, val iconId: Int, val route: String){
-    object ListItem: BottomNavItem("List", R.drawable.list_icon, Routes.SHOPPING_LIST)
-    object Receipt: BottomNavItem("Receipt", R.drawable.receipt_icon, Routes.RECEIPT_LIST)
+    object ListItem: BottomNavItem("Список", R.drawable.list_icon, Routes.SHOPPING_LIST)
+    object Receipt: BottomNavItem("Чек", R.drawable.receipt_icon, Routes.RECEIPT_LIST)
     object AboutItem: BottomNavItem("About", R.drawable.about_icon, Routes.ABOUT)
-    object NoteItem: BottomNavItem("Note", R.drawable.note_icon, Routes.NOTE_LIST)
-    object SettingsItem: BottomNavItem("Settings", R.drawable.settings_icon, Routes.SETTINGS)
+    object NoteItem: BottomNavItem("Заметки", R.drawable.note_icon, Routes.NOTE_LIST)
+    object SettingsItem: BottomNavItem("Настройки", R.drawable.settings_icon, Routes.SETTINGS)
 }
